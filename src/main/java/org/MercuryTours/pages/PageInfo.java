6e12c9029a2cc9b86c12d9b2b195a5d90ecdd056
siva <swaroop.om@gmail.com>
2017-10-03 17:14:41 +1100

    Changes made to support page object model

--- src/main/java/org/MercuryTours/pages/PageInfo.java
+++ src/main/java/org/MercuryTours/pages/PageInfo.java
@@ -1,5 +1,7 @@
 package org.MercuryTours.pages;
 
+import java.util.ResourceBundle;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,38 +11,45 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CommonUtil {
+public class PageInfo {
 	
-	WebDriver driver = null;
-	WebDriverWait wait = null;
-	
-public CommonUtil (String URL, String waitingtime, String browser){
-	
+	static WebDriver driver = null;
+	static WebDriverWait wait = null;
+	ResourceBundle pageprop =ResourceBundle.getBundle("pageproperties");
+//public CommonUtil (String URL, String waitingtime, String browser){
+	public PageInfo()
+	{
+		if(driver == null)
+		{
+		String URL = pageprop.getString("url");
+		String waitingtime = pageprop.getString("waitingtime");
+		String browser = pageprop.getString("browser");
 	if (browser.equalsIgnoreCase("chrome"))
 	{
 		driver= new ChromeDriver();
 	}
 	
-	else if (browser.equalsIgnoreCase("internet explorer"))
+	else if (browser.equalsIgnoreCase("internetExplorer"))
 	{
 		driver = new InternetExplorerDriver();
 	}
 	else if (browser.equalsIgnoreCase("FireFox"))
 	{
-		System.setProperty("webdriver.gecko.driver", "C:\\swaroop\\Selenium\\geckodriver-v0.18.0-win64\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver", pageprop.getString("browserdriver"));
 		driver = new FirefoxDriver();
 	}
 	
 	else 
 	{
-		System.setProperty("webdriver.gecko.driver", "C:\\swaroop\\Selenium\\geckodriver-v0.18.0-win64\\geckodriver.exe");
+		//No browsers selected, take the default browser
+		System.setProperty("webdriver.gecko.driver", pageprop.getString("browserdriver"));
 		driver = new FirefoxDriver();
 		System.out.println("no other browser selected, Firefox initiated");
 	}
 
 	wait = new WebDriverWait(driver, Integer.parseInt(waitingtime));
 	driver.get(URL);
-	
+		}
 	}
 
 public void txtField(String xpath, String text)
